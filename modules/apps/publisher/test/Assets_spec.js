f963967b9977cc2c2639f027723ce3ebe8ef9821
Ayesha <ayeshad.09@cse.mrt.ac.lk>
2014-11-10 11:09:00 +0530

    update copyright in publisher/test

--- modules/apps/publisher/test/Assets_spec.js
+++ modules/apps/publisher/test/Assets_spec.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ * Copyright (c) 2014, WSO2 Inc. (http://wso2.com) All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 var conf = require("conf.json");
 var authenticate, sessionId;
 var assetId;// = '7e0f7270-30b6-4993-86a2-8ea4cb59f5d3';
